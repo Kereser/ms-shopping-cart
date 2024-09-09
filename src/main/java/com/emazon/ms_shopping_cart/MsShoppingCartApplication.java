36b4316b19ea5a1fe9bc50c4fde388a76782f2dc
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-08 22:22:17 -0500

    Controller and application

---
+++ src/main/java/com/emazon/ms_shopping_cart/MsShoppingCartApplication.java
@@ -0,0 +1,13 @@
+package com.emazon.ms_shopping_cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsShoppingCartApplication.class, args);
+	}
+
+}
