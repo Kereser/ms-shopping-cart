b92d4dbac15fc39791b89c2eb6386a8a6a701233
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-29 17:13:21 -0500

    Update stock endpoints on feign port

--- src/main/java/com/emazon/ms_shopping_cart/domain/spi/StockFeignPort.java
+++ src/main/java/com/emazon/ms_shopping_cart/domain/spi/StockFeignPort.java
@@ -8,18 +8,16 @@ import com.emazon.ms_shopping_cart.application.dto.out.ArticleResDTO;
 import com.emazon.ms_shopping_cart.infra.config.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Set;
 
 @FeignClient(name = ConsUtils.MS_STOCK, url = ConsUtils.MS_STOCK_URL, configuration = FeignConfig.class)
 public interface StockFeignPort {
 
-    @PutMapping(value = ConsUtils.WITH_ARTICLES_URL, consumes = MediaType.APPLICATION_JSON_VALUE)
-    void handleAdditionToCart(ItemsReqDTO dto);
+    @PostMapping(value = ConsUtils.VALIDATE_STOCK_FOR_CART, consumes = MediaType.APPLICATION_JSON_VALUE)
+    void makeStockValidations(ItemsReqDTO dto);
 
     @GetMapping(value = ConsUtils.GET_ALL_ITEMS_FROM_STOCK, consumes = MediaType.APPLICATION_JSON_VALUE)
     PageDTO<ArticleResDTO> getPageableArticles(@PathVariable String articleIds,
@@ -30,4 +28,13 @@ public interface StockFeignPort {
 
     @GetMapping(value = ConsUtils.GET_ITEMS_WITH_PRICE, consumes = MediaType.APPLICATION_JSON_VALUE)
     Set<ArticlesPriceDTO> getArticlesPrice(@PathVariable String articleIds);
+
+    @PostMapping(value = ConsUtils.PROCESS_PURCHASE_ON_STOCK, consumes = MediaType.APPLICATION_JSON_VALUE)
+    void reduceStock(ItemsReqDTO itemsReqDTO);
+
+    @PostMapping(value = ConsUtils.GET_ALL_ARTICLES_FROM_STOCK, consumes = MediaType.APPLICATION_JSON_VALUE)
+    List<ArticleResDTO> getAllArticles(ItemsReqDTO itemsReqDTO);
+
+    @PostMapping(value = ConsUtils.PROCESS_CART_ROLLBACK_URL, consumes = MediaType.APPLICATION_JSON_VALUE)
+    void processRollbackOnStock(ItemsReqDTO itemsReqDTO);
 }
