4ed1714224c725ac52120a865c42df8bc1f0f8cd
Juan Sebastian Urrego <urregoarr00@gmail.com>
2024-09-29 17:09:53 -0500

    New field cartQuantity to differentiate total quantity

--- src/main/java/com/emazon/ms_shopping_cart/application/dto/out/ArticleResDTO.java
+++ src/main/java/com/emazon/ms_shopping_cart/application/dto/out/ArticleResDTO.java
@@ -3,6 +3,7 @@ package com.emazon.ms_shopping_cart.application.dto.out;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.FieldNameConstants;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -11,12 +12,14 @@ import java.util.Set;
 @Getter
 @Setter
 @Builder
+@FieldNameConstants
 public class ArticleResDTO {
     private Long id;
     private String name;
     private String description;
     private BigDecimal price;
     private Long quantity;
+    private Long cartQuantity;
     private Set<CategoryArticleResDTO> categories;
     private BrandResDTO brand;
     private LocalDateTime updatedAt;
